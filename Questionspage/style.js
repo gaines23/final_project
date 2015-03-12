96f11c36a411038b3c45d09f5cbf975d6be9b448
Melissa Gaines <gaines.melissa23@gmail.com>
2015-03-12 15:18:28 -0400

    added video

--- Questionspage/style.js
+++ Questionspage/style.js
@@ -2,3 +2,12 @@
 function myFunction(){
 	alert("You're Leaving The Page!");
 }
+
+
+$(document).ready(function(){
+$("#P1").hover(function(){
+	$("#P1").css("background-color", "blue");
+	}, function(){
+	$("#P1").css("background-color", "red");
+	});
+});
